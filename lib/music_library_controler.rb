e767d8bfa34c78cc4263428265d27c645fa69e45
Daiane Bushey <daianebushey@gmail.com>
2018-04-12 18:07:49 +0000

    automatically backed up by learn

--- lib/music_library_controler.rb
+++ lib/music_library_controler.rb
@@ -80,7 +80,7 @@ class MusicLibraryController
   def play_song
     #input = ""
     #while input < 1 && input > 5
-      #list_songs
+      list_songs
       puts "Which song number would you like to play?"
       input = gets.strip
       # case input
